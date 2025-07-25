ba059f875a0c8edafb5ce8d889fe49be6accc485
sigmond.cardozo <sigcardozo88.sc@gmail.com>
2025-07-26 03:48:01 +0530

    add files

---
+++ src/main/java/driver/DriverFactory.java
@@ -0,0 +1,70 @@
+package driver;
+
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DriverFactory {
+    private static ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        if (webDriver.get() == null) {
+            webDriver.set(createDriver());
+        }
+        return webDriver.get();
+    }
+
+    private static WebDriver createDriver() {
+        WebDriver driver = null;
+
+        switch (getBrowserType()) {
+            case "chrome" -> {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                driver = new ChromeDriver(chromeOptions);
+                System.out.println("chrome");
+                break;
+            }
+            case "firefox" -> {
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                driver = new FirefoxDriver(firefoxOptions);
+                System.out.println("firefox");
+                break;
+            }
+        }
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    private static String getBrowserType() {
+        String browserType = null;
+        String browserTypeRemoteValue = System.getProperty("browserType");
+
+        try {
+            if (browserTypeRemoteValue == null || browserTypeRemoteValue.isEmpty()) {
+                Properties properties = new Properties();
+                FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/main/java/properties/config.properties");
+                properties.load(file);
+                browserType = properties.getProperty("browser").toLowerCase().trim();
+            } else {
+                browserType = browserTypeRemoteValue;
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return browserType;
+    }
+
+    public static void cleanupDriver() {
+        webDriver.get().quit();
+        webDriver.remove();
+    }
+}
